0da65a1b8e13930d663b90a1501e9eb94f228de8
jonathanhansson <jonathanhanssondg@gmail.com>
2025-01-15 14:29:53 +0100

    All the methods required for the game to run

--- SnakeGame.cs
+++ SnakeGame.cs
@@ -11,6 +11,8 @@ class SnakeGame
 
     Position fruit;
 
+    Position direction = new Position(0, 1);
+
     public SnakeGame()
     {
         snake.Add(new Position(5, 5));
@@ -35,34 +37,40 @@ class SnakeGame
             }
         }
     }
-    public void DrawBoard()
-    {
-        Console.Clear();
+   public void DrawBoard()
+{
+    Console.Clear();
 
-        for (int i = 0; i < BoardHeight + 2; i++)
+    for (int i = 0; i < BoardHeight + 2; i++)
+    {
+        for (int j = 0; j < BoardWidth + 2; j++) 
         {
-            for (int j = 0; j < BoardWidth + 2; j++) 
+            if (i == 0 || i == BoardHeight + 1 || j == 0 || j == BoardWidth + 1)
             {
-                if (i == 0 || i == BoardHeight + 1 || j == 0 || j == BoardWidth + 1)
-                {
-                    Console.Write('#'); // Rita kanterna
-                }
-                else if (IsSnake(i - 1, j - 1)) // Justera för att matcha insidan
-                {
-                    Console.Write('O'); // Rita ormen
-                }
-                else if (i - 1 == fruit.Y && j - 1 == fruit.X) // Rita frukten
-                {
-                    Console.Write('X');
-                }
-                else
-                {
-                    Console.Write(' '); // Tom insida
-                }
+                Console.Write('#'); // Rita kanterna
             }
-            Console.WriteLine();
+            else if (IsSnake(i - 1, j - 1)) // Justera för att matcha insidan
+            {   
+                Console.Write('O'); // Rita ormen
+            }
+            else
+            {
+                Console.Write(' '); // Tom insida
+            }
         }
+
+        // När hela raden är klar, kontrollera om frukten ska ritas på denna rad
+        if (i - 1 == fruit.Y)
+        {
+            Console.SetCursorPosition(fruit.X + 1, i); // Justera för kanterna
+            Console.Write('X');  // Rita frukten
+        }
+
+        Console.WriteLine();
     }
+}
+
+
 
 
     public bool IsSnake(int y, int x)
@@ -82,5 +90,89 @@ class SnakeGame
     {
         Random rnd = new Random();
         fruit = new Position(rnd.Next(0, BoardWidth), rnd.Next(0, BoardHeight));
+    }
+
+    public void MoveSnake()
+    {
+        Position head = snake[0];
+
+        Position newHead = new Position(head.X + direction.X, head.Y + direction.Y);
+
+        if (newHead.X == fruit.X && newHead.Y == fruit.Y)
+        {
+            snake.Insert(0, newHead);
+            GenerateFruit();
+        }
+        else
+        {
+            snake.Insert(0, newHead);
+            snake.RemoveAt(snake.Count - 1);
+        }
+
+
+    }
+
+    public void HandleInput()
+    {
+        if (Console.KeyAvailable)
+        {
+            var key = Console.ReadKey(true).Key;
+
+            if (key == ConsoleKey.UpArrow && direction.Y != 1)
+            {
+                direction = new Position(0, -1);
+            }
+            else if (key == ConsoleKey.DownArrow && direction.Y != -1)
+            {
+                direction = new Position(0, 1);
+            }
+            else if (key == ConsoleKey.LeftArrow && direction.X != 1)
+            {
+                direction = new Position(-1, 0);
+            }
+            else if (key == ConsoleKey.RightArrow && direction.X != -1)
+            {
+                direction = new Position(1, 0);
+            }
+        }
+    }
+
+    public bool CheckCollision()
+    {
+        Position head = snake[0];
+
+        // Kolla om huvudet träffar väggarna
+        if (head.X < 0 || head.X >= BoardWidth || head.Y < 0 || head.Y >= BoardHeight)
+        {
+            return true;
+        }
+
+        // Kolla om huvudet träffar någon del av kroppen (utom det första segmentet, som är huvudet)
+        for (int i = 1; i < snake.Count; i++)
+        {
+            if (snake[i].X == head.X && snake[i].Y == head.Y)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+
+    public void RunGame()
+    {
+        while (true)
+        {
+            HandleInput();
+            MoveSnake();
+            if (CheckCollision())
+            {
+                System.Console.WriteLine("Game over!");
+                break;
+            }
+            DrawBoard();
+            Thread.Sleep(120);
+        }
     }
 }
